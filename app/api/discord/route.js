41217eec29834e144034f4963e1fd60e3cc5001c
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2024-06-05 20:06:18 +0100

    remove unwanted console.debug

--- app/api/discord/route.js
+++ app/api/discord/route.js
@@ -12,8 +12,6 @@ export const dynamic = 'force-dynamic'
 export const POST = async req => {
   const interaction = await req.json()
 
-  console.debug('interaction %O', interaction)
-
   if (interaction.type !== InteractionType.APPLICATION_COMMAND) {
     return NextResponse.json({
       type: InteractionResponseType.PONG
